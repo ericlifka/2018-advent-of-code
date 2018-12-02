29174863335965da078f111497f2a524327f0c5a
Eric Lifka <eric.lifka@gmail.com>
2018-12-02 11:50:40 -0500

    day1 part2 first pass

--- 01.js
+++ 01.js
@@ -1,8 +1,29 @@
 let input = require('./input/01')
 
-let answer = input
+let frequencies = 
+input
     .split('\n')
     .map((n => parseInt(n, 10)))
+
+let answer1 = 
+frequencies
     .reduce((accum, curr) => accum + curr, 0)
 
-console.log(answer)
+console.log('answer1', answer1)
+
+let seen = { }
+let f = 0
+let loop = 0
+while (true) {
+    for (let i of frequencies) {
+        f += i
+        if (seen [ f ]) {
+            console.log('answer2', f);
+            return
+        }
+        else {
+            seen[ f ] = true
+        }
+    }
+    console.log(loop++)
+}
