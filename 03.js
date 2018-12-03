15a56a8c3a6c7dffb137ca8dd76a52004bc5ed33
Eric Lifka <eric.lifka@gmail.com>
2018-12-03 09:59:42 -0500

    day 3 part 2

--- 03.js
+++ 03.js
@@ -3,15 +3,18 @@ Array.from(Array(1000))
     .map(() => Array.from(Array(1000))
                     .map(() => 0))
 
+let instructions =
 require('./input/03')
     .split('\n')
     .map(line => line.split(/[@,:x ]+/))
-    .map(data => data.map(i => parseInt(i, 10)))
-    .forEach(([id, left, top, width, height]) => {
-        for(let x = left; x < left + width; x++)
-            for (let y = top; y < top + height; y++)
-                fabric[x][y]++
-    })
+    .map(([id, ...data]) => [id, ...data.map(i => parseInt(i, 10))])
+
+instructions
+.forEach(([id, left, top, width, height]) => {
+    for(let x = left; x < left + width; x++)
+        for (let y = top; y < top + height; y++)
+            fabric[x][y]++
+})
 
 let squaresOverlapped =
 fabric.reduce((count, row) => 
@@ -21,3 +24,14 @@ fabric.reduce((count, row) =>
 , 0)
 
 console.log('answer1', squaresOverlapped)
+
+let [id] =
+instructions.find(([id, left, top, width, height]) => {
+    for(let x = left; x < left + width; x++)
+        for (let y = top; y < top + height; y++)
+            if (fabric[x][y] > 1)
+                return false
+    return true
+})
+
+console.log('answer2', id)
