23412855a67c997718d453628a3cdd193a993f66
Eric Lifka <eric.lifka@gmail.com>
2018-12-07 11:07:44 -0500

    day 7 part 2

--- 07.js
+++ 07.js
@@ -5,7 +5,7 @@ require('./input/07')
    .map( desc => [ desc[ 1 ], desc[ 7 ] ] ) 
 
 let nodes = { }
-const newNode = (name) => ({ name, before: [], after: [], visited: false })
+const newNode = (name) => ({ name, before: [], after: [] })
 const getNode = (name) =>
     nodes[ name ]
         ? nodes[ name ]
@@ -22,7 +22,6 @@ dependencies
 
 let order = ''
 let queue = Object.keys(nodes).filter( n => nodes[ n ].before.length == 0 )
-let unfinished = [ ]
 
 while ( queue.length > 0 ) {
     queue.sort()
@@ -35,7 +34,37 @@ while ( queue.length > 0 ) {
     node.after.forEach( n =>
         nodes[ n ].before.reduce( (accum, parent) => nodes[ parent ].visited && accum, true )
             ? queue.push(n)
-            : unfinished.push(n) )
+            : null )
 }
 
 console.log('part 1', order)
+
+const PROCESSING_ORDER = '_ABCDEFGHIJKLMNOPQRSTUVWXYZ', 
+      HELPERS = 5
+let finished = [],
+    active = [],
+    second = 0,
+    pending = Object.keys(nodes).filter( n => nodes[ n ].before.length == 0 )
+
+Object.keys(nodes).forEach(n => nodes[n].processingLeft = 60 + PROCESSING_ORDER.indexOf(n))
+
+while ( active.length > 0 || pending.length > 0 ) {
+    pending.sort()
+
+    while ( active.length < HELPERS && pending.length > 0 )
+        active.push( pending.shift() )
+
+    finished = active.filter( n => --nodes[ n ].processingLeft <= 0 )
+    active = active.filter( n => nodes[ n ].processingLeft > 0 )
+
+    finished.forEach( n => (
+        nodes[ n ].finished = true,
+        nodes[ n ].after.forEach( n =>
+            nodes[ n ].before.reduce( (accum, parent) => nodes[ parent ].finished && accum, true )
+                ? pending.push(n)
+                : null )))
+
+    second++
+}
+
+console.log(`part 2 ${second} seconds`)
