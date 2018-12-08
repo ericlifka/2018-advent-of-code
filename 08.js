eda88ebfe30f961b05a9a46a6d9a73efec5042b9
Eric Lifka <eric.lifka@gmail.com>
2018-12-08 02:24:36 -0500

    "cleanup"

--- 08.js
+++ 08.js
@@ -2,48 +2,34 @@ let numbers =
 require('./input/08.js')
 // `2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2`
     .split(' ')
-    .map(i => parseInt(i, 10))
+    .map(Number)
 
-let metadata_collection = [ ]
+let metadataSum = 0
 
-function processNode(numbers, index) {
+function processNode(index = 0) {
     let children = numbers[ index++ ],
         metadata = numbers[ index++ ],
         childValues = [ 0 ],
-        metaDataValues = [ ],
-        value = 0
+        metadataValues = [ ]
 
-    for (let i = 1; i <= children; i++) {
-        [ index, childValues[ i ] ] = processNode(numbers, index)
-    }
+    for (let i = 1; i <= children; i++)
+        [ index, childValues[ i ] ] = processNode(index)
 
-    for (let j = 0; j < metadata; j++) {
-        metaDataValues.push( numbers[ index++ ] )
-    }
-    console.log(childValues, metaDataValues)
+    for (let j = 0; j < metadata; j++)
+        metadataValues.push( numbers[ index++ ] )
 
-    metadata_collection = metadata_collection.concat( metaDataValues )
+    metadataSum += metadataValues.reduce( (sum, i) => sum + i, 0 )
 
-    if (children == 0) {
-        value = metaDataValues.reduce( (sum, i) => sum + i, 0 )
-    } else {
-        value = metaDataValues.reduce( (sum, m) => {
-            if ( m < childValues.length ) {
-                return sum + childValues[ m ]
-            } else {
-                return sum
-            }
-        }, 0 )
-    }
-
-    // console.log([index, value])
-    return [ index, value ]
+    return [ index,
+            !children ? metadataValues.reduce( (sum, i) => sum + i, 0 )
+                      : metadataValues.reduce( (sum, m) =>
+                            m < childValues.length
+                                ? sum + childValues[ m ]
+                                : sum
+                        , 0 ) ]
 }
 
-let [ _, parentValue ] =
-processNode(numbers, 0)
+let [ _, parentValue ] = processNode()
 
-let sum =
-metadata_collection.reduce( (accum, i) => accum + i, 0 )
-console.log('part 1', sum)
+console.log('part 1', metadataSum)
 console.log('part 2', parentValue)
