419894d239af1b2c7a5f212642825bc51fdeecd3
Eric Lifka <eric.lifka@gmail.com>
2018-12-08 20:37:18 -0500

    some cleanup

--- clj/01.clj
+++ clj/01.clj
@@ -1,27 +1,26 @@
 (ns advent.2018.01
     (:require [clojure.string :refer [split-lines]]))
+  
+(defn part1 [frequencies]
+  (reduce + frequencies))
+
+(defn run-sequence [seen sum frequencies]
+  (let [new-sum (+ sum (first frequencies))
+        remaining (rest frequencies)]
+    (cond
+      (seen new-sum) [seen new-sum]
+      (<= (count remaining) 0) [seen new-sum]
+      :else (run-sequence (conj seen new-sum) new-sum remaining))))
+
+(defn part2 [seen sum frequencies]
+  (let [[seen sum] (run-sequence seen sum frequencies)]
+    (if (seen sum)
+      sum
+      (part2 seen sum frequencies))))
 
 (let [input (slurp "input-01.txt")
       lines (split-lines input)
       numbers (map #(Integer/parseInt %) lines)]
   
-  (defn part1 []
-    (reduce + numbers))
-
-  (defn run-sequence [seen sum frequencies]
-    (let [new-sum (+ sum (first frequencies))
-          remaining (rest frequencies)]
-         (if (seen new-sum)
-           [seen new-sum]
-           (if (> (count remaining) 0)
-             (run-sequence (conj seen new-sum) new-sum remaining)
-             [seen new-sum]))))
-  
-  (defn part2 [seen sum frequencies]
-    (let [[seen sum] (run-sequence seen sum frequencies)]
-         (if (seen sum)
-           sum
-           (part2 seen sum frequencies))))
-  
-  (println (part1))
-  (println (part2 #{} 0 numbers)))
+  (println "part1" (part1 numbers))
+  (println "part2" (part2 #{} 0 numbers)))
