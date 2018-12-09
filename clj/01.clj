3a11955c2b2947a10cd14d44385d9a0704963493
Eric Lifka <eric.lifka@gmail.com>
2018-12-08 20:27:44 -0500

    day 1 part 2 in clojure

--- clj/01.clj
+++ clj/01.clj
@@ -1,10 +1,27 @@
 (ns advent.2018.01
     (:require [clojure.string :refer [split-lines]]))
 
-(defn part1 []
-  (let [input (slurp "input-01.txt")
-        lines (split-lines input)
-        numbers (map #(Integer/parseInt %) lines)]
-    (reduce + numbers)))
+(let [input (slurp "input-01.txt")
+      lines (split-lines input)
+      numbers (map #(Integer/parseInt %) lines)]
+  
+  (defn part1 []
+    (reduce + numbers))
 
-(println (part1))
+  (defn run-sequence [seen sum frequencies]
+    (let [new-sum (+ sum (first frequencies))
+          remaining (rest frequencies)]
+         (if (seen new-sum)
+           [seen new-sum]
+           (if (> (count remaining) 0)
+             (run-sequence (conj seen new-sum) new-sum remaining)
+             [seen new-sum]))))
+  
+  (defn part2 [seen sum frequencies]
+    (let [[seen sum] (run-sequence seen sum frequencies)]
+         (if (seen sum)
+           sum
+           (part2 seen sum frequencies))))
+  
+  (println (part1))
+  (println (part2 #{} 0 numbers)))
