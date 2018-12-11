43272a415c5ae4f43b661c06eb2e98337fb60126
Eric Lifka <eric.lifka@gmail.com>
2018-12-10 20:28:36 -0500

    part 1

--- js/10.js
+++ js/10.js
@@ -5,8 +5,6 @@ require('./input/10')
     .map(parts => ([parts[1], parts[2], parts[4], parts[5]]))
     .map(line => line.map(Number))
 
-
-
 function printGrid(data) {
 
     let xs = data.map(parts => parts[0]),
@@ -16,22 +14,23 @@ function printGrid(data) {
         maxY = Math.max.apply(null, ys),
         minY = Math.min.apply(null, ys)
 
+    console.log(`x: ${minX}-${maxX}, y: ${minY}-${maxY}`)
+
     let grid = []
-    for (let y = minY; y <= maxY; y++) {
-        grid[y] = []
-        for (let x = minX; x <= maxX; x++) {
-            grid[y][x] = '.'
-        }
-    }
 
     data.forEach(([x, y]) => {
-        grid[y][x] = '#'
+        grid[`${x}.${y}`] = true
     })
 
     for (let y = minY; y <= maxY; y++) {
         let line = ''
         for (let x = minX; x <= maxX; x++) {
-            line += grid[y][x]
+            if (grid[`${x}.${y}`]){
+                line += '#'
+            } else {
+                line += ' '
+            }
+            
         }
         console.log(line)
     }
@@ -44,10 +43,34 @@ function stepGrid(data) {
     })
 }
 
-printGrid(input)
-stepGrid(input)
-printGrid(input)
-stepGrid(input)
-printGrid(input)
-stepGrid(input)
+function findMinimumStep(data) {
+
+    let minRect = {area: 10000000000000, timeStep: -1}
+
+    for (let i = 0; i < 20000; i++) {
+        let xs = data.map(parts => parts[0]),
+            ys = data.map(parts => parts[1]),
+            maxX = Math.max.apply(null, xs),
+            minX = Math.min.apply(null, xs),
+            maxY = Math.max.apply(null, ys),
+            minY = Math.min.apply(null, ys),
+            area = (maxX - minX) * (maxY - minY)
+
+        if (area < minRect.area) {
+            minRect.area = area
+            minRect.timeStep = i
+        }
+        else {
+            break
+        }
+
+        stepGrid(data)
+    }
+
+    return minRect
+} // found min to be at step 10003
+
+for (let i = 0; i < 10003; i++) {
+    stepGrid(input)
+}
 printGrid(input)
