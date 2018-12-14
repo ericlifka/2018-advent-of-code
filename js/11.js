1af3032f4d3e465e4a8bfd6b89e8fc5aaa845d1f
Eric Lifka <eric.lifka@gmail.com>
2018-12-14 12:15:15 -0500

    cleanup on new solution for 11.js

--- js/11.js
+++ js/11.js
@@ -2,7 +2,7 @@ const startTime = new Date()
 const GRID_CERIAL_NUMBER = 5468
 
 const empty = size => Array.from(Array(size))
-const table = (size, val = () => 0) => 
+const table = (size, val) =>
     empty(size).map((_, y) => empty(size).map((_, x) =>
         val(x, y)))
 
@@ -11,50 +11,53 @@ const getHundredsPlace = x =>
         ? 0
         : Number( `${x}`.slice(-3, -2) )
 
-function calcPowerLevel(x, y, gridCerial = GRID_CERIAL_NUMBER) {
-    let rackId = x + 10,
-        powerLevel = rackId * y 
+const calcPowerLevel = (x, y, rackId = x + 10) =>
+    getHundredsPlace( ( rackId * y  + GRID_CERIAL_NUMBER ) * rackId ) - 5
 
-    powerLevel += gridCerial
-    powerLevel *= rackId
+let powerGrid = table(301, (x, y) => calcPowerLevel(x, y)),
+    sumTable = table(301, () => 0)
 
-    return getHundredsPlace(powerLevel) - 5
-}
+for ( let y = 1, rowSum = 0; y <= 300; y++, rowSum = 0 )
+    for ( let x = 1; x <= 300; x++ )
+        sumTable[ y ][ x ] = ( rowSum += powerGrid[ y ][ x ] ) + sumTable[ y - 1 ][ x ]
 
-let powerGrid = table(301, (x, y) => 
-    (x == 0 || y == 0)
-        ? 0
-        : calcPowerLevel(x, y))
+const dataPrepTime = new Date()
 
-let sumTable = table(301)
+let threeMax = { x: 0, y: 0, s: 3, sum: 0 }
+for ( let y = 1; y <= 298; y++ )
+    for ( let x = 1; x <= 298; x++ ) {
+        let upper = sumTable[ y - 1 ][ x - 1 ],
+            lower = sumTable[ y + 2 ][ x + 2 ],
+            left = sumTable[ y + 2 ][ x - 1 ],
+            top = sumTable[ y - 1 ][ x + 2 ],
+            sum = lower + upper - left - top
 
-for ( let y = 1; y <= 300; y++ ) {
-    let rowSum = 0
-
-    for ( let x = 1; x <= 300; x++ ) {
-        rowSum += powerGrid[ y ][ x ]
-
-        sumTable[ y ][ x ] = rowSum + sumTable[ y - 1 ][ x ]
+        if ( sum > threeMax.sum )
+            threeMax = { x, y, s: 3, sum }
     }
-}
 
-let max = { x: 0, y: 0, s: 1, sum: 0 }
-for ( let y = 1; y <= 300; y++ ) {
-    for ( let x = 1; x <= 300; x++ ) {
-        let upper = sumTable[ y - 1 ][ x - 1 ]
-        for ( let s = 0; s + x <= 300 && s + y <= 300; s++ ) {
-            let lower = sumTable[ y + s ][ x + s ],
-                
+const part1Time = new Date()
+
+let allMax = threeMax
+for ( let y = 1; y <= 297; y++ )
+    for ( let x = 1; x <= 297; x++ )
+        for ( let s = 3; s + x <= 300 && s + y <= 300; s++ ) {
+            let upper = sumTable[ y - 1 ][ x - 1 ],
+                lower = sumTable[ y + s ][ x + s ],
                 left = sumTable[ y + s ][ x - 1 ],
                 top = sumTable[ y - 1 ][ x + s ],
                 sum = lower + upper - left - top
 
-            if ( sum > max.sum ) {
-                max = { x, y, s: s + 1, sum }
-            }
-        }
-    }
-}
+            if ( sum > allMax.sum )
+                allMax = { x, y, s: s + 1, sum }
 
-console.log('part 2', max)
-console.log('time: ', new Date() - startTime)
+            if (sum < 0)
+                break
+        }
+
+const part2Time = new Date()
+
+console.log(`Data prep: ${dataPrepTime - startTime}ms`)
+console.log(`Part 1 time: ${part1Time - dataPrepTime}ms, ${JSON.stringify(threeMax)}`)
+console.log(`Part 2 time: ${part2Time - part1Time}ms, ${JSON.stringify(allMax)}`)
+console.log(`Total time: ${part2Time - startTime}ms`)
