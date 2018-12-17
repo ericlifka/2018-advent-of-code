edf44ff2ac10a093b830ecaeced5e732d8861b3b
Eric Lifka <eric.lifka@gmail.com>
2018-12-17 09:47:19 -0500

    day 13 part 2 clojure

--- clj/13.clj
+++ clj/13.clj
@@ -1,30 +1,35 @@
 (ns advent.2018.01
-  (:require [clojure.string :refer [split-lines 
-                                    join]]))
+  (:require [clojure.string :refer [split-lines]]))
+
+(defn map-2d [lambda grid]
+  (map (fn [row]
+         (map (fn [elem]
+                (lambda elem))
+              row))
+       grid))
+
+(defn lookup [seq-2d x y]
+  (-> seq-2d
+      (nth y)
+      (nth x)))
 
 (defn print-grid [grid]
   (doall (for [row grid]
            (do
-             (doall  (for [char row] 
-                       (print char)))
+             (doall (for [char row] 
+                      (print char)))
              (println)))))
 
 (defn print-tracks [tracks karts]
   (print-grid
    (reduce (fn [updated-tracks kart] 
-             (assoc-in updated-tracks 
-                       [(:y kart) (:x kart)] 
+             (assoc-in updated-tracks [(:y kart) (:x kart)] 
                        (condp = (:direction kart)
                          :north \^
                          :south \v
                          :east \>
-                         :west \<)
-                       ))
-           tracks
-           (vec karts))))
-
-(defn lookup [seq-2d x y]
-  (nth (nth seq-2d y) x))
+                         :west \<)))
+           tracks karts)))
 
 (defn find-karts [grid]
   (remove 
@@ -38,14 +43,6 @@
          (= cell \<) {:direction :west :turns 0 :x x :y y}
          (= cell \^) {:direction :north :turns 0 :x x :y y}
          :else nil)))))
-
-(defn map-2d [lambda grid]
-  (vec (doall (map 
-               (fn [row]
-                 (vec (doall (map (fn [elem]
-                                    (lambda elem))
-                                  row))))
-               grid))))
 
 (defn remove-karts [grid]
   (map-2d #(cond
@@ -96,38 +93,43 @@
                                    (= :west direction) {:direction :north :turns 0 :x x :y (dec y)})))))
 
 (defn check-collision [kart other-karts]
-  (reduce #(or %1 %2)
-          (for [other other-karts]
-            (and (= (:x other) (:x kart))
-                 (= (:y other) (:y kart))))))
+  (let [other (first other-karts)
+        remaining (rest other-karts)]
+    (cond
+      (nil? other) nil
+      (and (= (:x other) (:x kart))
+           (= (:y other) (:y kart))) other
+      :else (recur kart remaining))))
 
 (let [input (slurp "input-13.txt")
       grid (vec (map #(vec (seq %)) (split-lines input)))
       all-karts (sort-karts (find-karts grid))
-      tracks (remove-karts grid)
-      ]
-
-  (println all-karts)
-  (print-tracks tracks all-karts)
-  
+      tracks (remove-karts grid)]
   (loop [processed-karts []
          current-kart (first all-karts)
          remaining-karts (rest all-karts)]
-    ; (print-tracks tracks (conj (concat processed-karts remaining-karts) current-kart))
-    ; (println processed-karts current-kart remaining-karts)
-    (let [updated-kart (step-kart current-kart tracks)]
-      ; (println updated-kart)
-      (if (check-collision updated-kart (concat processed-karts remaining-karts))
-        (println updated-kart) ; found crash, print and end
-        (let [rollover? (zero? (count remaining-karts))
-              new-processed-karts (if rollover? 
-                                    [] 
-                                    (conj processed-karts updated-kart))
-              new-remaining-karts (if rollover? 
-                                    (sort-karts (conj processed-karts updated-kart)) 
-                                    remaining-karts)]
-          (recur new-processed-karts
-                 (first new-remaining-karts)
-                 (rest new-remaining-karts))
-          ))))
-  )
+    (let [; move the current kart one step
+          updated-kart (step-kart current-kart tracks)
+          ; find any kart that the current kart collided with
+          collision-kart (check-collision updated-kart (concat processed-karts remaining-karts))
+          ; update the lists based on whether there was a collision
+          processed-karts (if (nil? collision-kart)
+                            (conj processed-karts updated-kart)
+                            (remove #(= % collision-kart) processed-karts))
+          remaining-karts (if (nil? collision-kart)
+                            remaining-karts
+                            (remove #(= % collision-kart) remaining-karts))
+          ; check if we need to move the processed karts back into the queue to start a new tick
+          rollover? (zero? (count remaining-karts))
+          new-processed-karts (if rollover? [] processed-karts)
+          new-remaining-karts (if rollover? 
+                                (sort-karts processed-karts)
+                                remaining-karts)
+          ; check if we've hit the end of a full tick and we're down to one kart
+          end? (and rollover? (= 1 (count new-remaining-karts)))]
+      (if end?
+        (println (concat new-processed-karts new-remaining-karts)) ; only one kart left, print and end
+        (recur new-processed-karts
+               (first new-remaining-karts)
+               (rest new-remaining-karts))
+        ))))
