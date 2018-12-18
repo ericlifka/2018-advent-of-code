6b6f70ed5cce5cb9dd8e3fed8b00c36eaabfe5d5
Eric Lifka <eric.lifka@gmail.com>
2018-12-18 09:44:49 -0500

    day 14 js cleanup

--- js/14.js
+++ js/14.js
@@ -1,36 +1,29 @@
-const start = new Date()
-const TARGET = `846601`
-const TARGET_COUNT = TARGET.length
-
-function generateRecipes (val1, val2) {
-    return `${val1 + val2}`.split('').map(char =>
-        parseInt(char, 10))
-}
-
-let elf1 = 0,
+let start = new Date(),
+    TARGET = '846601',
+    TARGET_COUNT = TARGET.length,
+    elf1 = 0,
     elf2 = 1,
-    recipes = [3, 7]
+    recipes = [ 3, 7 ],
+    index = -1,
+    lastN, elf1Value, elf2Value, sum
 
-while (true) {
-    let elf1Value = recipes[ elf1 ],
-        elf2Value = recipes[ elf2 ],
-        newRecipes = generateRecipes( elf1Value, elf2Value )
+while ( index == -1 ) {
+    elf1Value = recipes[ elf1 ]
+    elf2Value = recipes[ elf2 ]
+    sum = elf1Value + elf2Value
 
-    newRecipes.forEach( recipe => recipes.push(recipe) )
+    if ( sum >= 10 )
+        recipes.push( 1 ),
+        recipes.push( sum % 10 )
+    else
+        recipes.push( sum )
 
     elf1 = (elf1 + elf1Value + 1) % recipes.length
     elf2 = (elf2 + elf2Value + 1) % recipes.length
 
-    let last = recipes.slice(-TARGET_COUNT).join('')
-    let last2 = recipes.slice(-TARGET_COUNT - 1, -1).join('')
-    
-    if ( last == TARGET ) {
-        console.log( recipes.length - TARGET_COUNT )
-        break
-    } else if ( last2 == TARGET  ) {
-        console.log( recipes.length - TARGET_COUNT - 1 )
-        break
-    }
+    lastN = recipes.slice( -TARGET_COUNT - 1 ).join('')
+    index = lastN.indexOf( TARGET )
 }
 
-console.log('time: ', new Date() - start)
+console.log( recipes.length - TARGET_COUNT - [ 1, 0 ][ index ] )
+console.log( 'time: ', new Date() - start )
