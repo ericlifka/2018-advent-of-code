ae5d71d2a4a139d3081806ff3dc0c4f174af26af
Eric Lifka <eric.lifka@gmail.com>
2018-12-18 11:05:14 -0500

    simple tweak to make 14 part 1 performant in clojure

--- clj/14.clj
+++ clj/14.clj
@@ -5,19 +5,23 @@
 (defn generate-recipes [val1 val2]
   (map #(Integer/parseInt (str %)) (seq (str (+ val1 val2)))))
 
+(defn add-recipes [recipes new-recipes]
+  (if (zero? (count new-recipes))
+    recipes
+    (recur (conj recipes (first new-recipes))
+           (rest new-recipes))))
+
+(time
 (loop [elf1 0
        elf2 1
        recipes [3 7]]
   (let [elf1-value (nth recipes elf1)
         elf2-value (nth recipes elf2)
         new-recipes (generate-recipes elf1-value elf2-value)
-        joined-recipes (concat recipes new-recipes)
-        recipe-count (count joined-recipes)
-        ]
-    (if (zero? (mod recipe-count 10000))
-      (println recipe-count))
+        joined-recipes (add-recipes recipes new-recipes)
+        recipe-count (count joined-recipes)]
     (if (<= TARGET recipe-count)
       (println (take-last 10 joined-recipes))
       (recur (mod (+ 1 elf1 elf1-value) recipe-count)
              (mod (+ 1 elf2 elf2-value) recipe-count)
-             joined-recipes))))
+             joined-recipes)))))
