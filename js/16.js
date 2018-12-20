b57633210de872d26d07bf63ad628a42f510f8f7
Eric Lifka <eric.lifka@gmail.com>
2018-12-20 01:13:49 -0500

    modified 16 to work out opcodes and solve part 2

--- js/16.js
+++ js/16.js
@@ -3,12 +3,21 @@ const logger = str => console.log(str)
 const len = entity => entity.length
 const update = (registers, reg, val, 
                 clone = registers.slice(0)) => (
-    clone[ reg ] = val, 
-    clone)
+                    clone[ reg ] = val, 
+                    clone)
 const compare = ([a1, ...a_rest], [b1, ...b_rest]) =>
-    a1 === undefined && b1 === undefined
-        ? true
-        : a1 === b1 && compare(a_rest, b_rest)
+                    a1 === undefined && b1 === undefined
+                        ? true
+                        : a1 === b1 && compare(a_rest, b_rest)
+const addInto = (key, value, collection) => (
+                    (collection[ key ]
+                        ? collection[ key ].push( value )
+                        : collection[ key ] = [ value ]),
+                    collection)
+const bucket = (get, [item, ...rest]) =>
+                    item === undefined
+                        ? {}
+                        : addInto( get(item), item, bucket(get, rest) )
 
 const addr = (regs, [op, r_a, r_b, r_c]) => update(regs, r_c, regs[ r_a ] + regs[ r_b ])
 const addi = (regs, [op, r_a, v_b, r_c]) => update(regs, r_c, regs[ r_a ] + v_b)
@@ -27,11 +36,11 @@ const eqir = (regs, [op, v_a, r_b, r_c]) => update(regs, r_c, v_a == regs[ r_b ]
 const eqri = (regs, [op, r_a, v_b, r_c]) => update(regs, r_c, regs[ r_a ] == v_b ? 1 : 0)
 const eqrr = (regs, [op, r_a, r_b, r_c]) => update(regs, r_c, regs[ r_a ] == regs[ r_b ] ? 1 : 0)
 
-const operations = [ addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr ]
-
+const opcodes = [ gtrr, borr, gtir, eqri, addr, seti, eqrr, gtri, 
+                  banr, addi, setr, mulr, bori, muli, eqir, bani ]
 const runTestCase = ({ before, instruction, after }) =>
-    3 <= len( operations.filter( operation => 
-                                    compare( after, operation(before, instruction) )))
+    1 == len( operations.filter( operation => 
+                                    compare( after, operation(before, instruction))))
 
 let [ p1input, p2input ] = require('./input/16').split('\n\n\n\n')
 
@@ -44,5 +53,10 @@ p1input.split('\n\n')
         after: eval( after.split(': ')[ 1 ] )
     }))
 
-console.log(p1_test_cases.length)
-console.log(len(p1_test_cases.filter(runTestCase)))
+let registers = [ 0, 0, 0, 0 ]
+p2input.split('\n')
+    .map(line => eval( `[${line.split(' ').join(', ')}]` ))
+    .forEach(instruction => (
+        registers = opcodes[ instruction[ 0 ] ](registers, instruction)))
+
+console.log(registers)
